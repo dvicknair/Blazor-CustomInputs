0acccbe5df9c5f1aa2f8b769a328bc3637a0e817
Donald Vicknair <dvicknair@gmail.com>
2024-08-31 16:11:02 -0500

    Add project files.

---
+++ Models/Widget.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomBlazorInputs.Models
+{
+    public class Widget
+    {
+        [Required]
+        public int? Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Option { get; set; }
+        [Required]
+        public DateTime? CreatedAt { get; set; }
+    }
+}
